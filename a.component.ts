952ca1daf7cce82c7f7bb754d0ee7047b3a1bd85
Esserhir <96774730+Esserhir@users.noreply.github.com>
2022-01-07 13:52:52 +0100

    Add files via upload

---
+++ a.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './a.component.html',
+  styleUrls: ['./a.component.css']
+})
+export class AppComponent  {
+
+  title= Management Ressources;
+
+
+  
+
+}
